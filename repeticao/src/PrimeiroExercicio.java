cc20d518bea80835d1b4f306da3208ff803fad1a
AleehSophia <alessia.casado@gmail.com>
2023-11-07 12:58:01 -0300

    Exercicio 7.3.1

---
+++ repeticao/src/PrimeiroExercicio.java
@@ -0,0 +1,16 @@
+/*
+* Exercicio 7.3.1 - Logica de programacao.
+* Escreva um algoritmo que, utilizando a estrutura de controle de
+* repetição para/fim_para, exiba na tela os números existentes entre 150 e
+* 250, inclusive estes.
+*/
+public class PrimeiroExercicio {
+    public static void main(String[] args) {
+
+        int n = 250;
+
+        for (int i = 150; i <= n; i++) {
+            System.out.println(i);
+        }
+    }
+}
